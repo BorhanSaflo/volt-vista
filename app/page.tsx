8fb7aef994a6f956f7309ddec0d578bd2e588d86
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-11-15 20:56:50 -0500

    v0.2.5 - fix: Set homepage video to play in line to avoid full screen on mobile.

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ export default function page() {
     <section className="bg-[#000] relative h-screen w-screen">
       <div className="z-0 top-0 right-0 absolute h-[100%] w-[100%] lg:w-[60%] opacity-40 lg:opacity-90">
         <div className="relative h-full w-full overflow-hidden">
-          <video muted loop autoPlay className="z-0 object-cover h-full w-full">
-            <source src="./videos/video.mp4" type="video/mp4" />
-          </video>
+          <video src="./videos/video.mp4" playsInline muted loop autoPlay controls={false} className="z-0 object-cover h-full w-full" />
           <div className="z-10 w-full h-full top-0 left-0 absolute bg-gradient-to-t lg:bg-gradient-to-r from-[#000] via-[#0000009e] to-[#0000006a]" />
         </div>
       </div>
@@ -17,7 +15,7 @@ export default function page() {
         }
       />
       <div
-        className="z-10 absolute w-full lg:w-auto p-10 top-1/2 left-1/2 lg:top-1/2 lg:left-10 transform -translate-x-1/2 -translate-y-1/2 lg:translate-x-0">
+        className="z-10 absolute w-full lg:w-auto p-10 top-1/2 left-1/2 lg:top-1/2 lg:left-10 3xl:left-60 transform -translate-x-1/2 -translate-y-1/2 lg:translate-x-0">
         <div className="flex flex-col justify-center gap-3 w-full items-center lg:items-start text-center lg:text-left lg:max-w-[350px]">
           <h1 className="text-[#bae2c6] font-bold lg:text-[5rem] text-5xl">Volt Vista</h1>
           <h2 className="text-[#BFDBC8] font-semibold lg:text-[1.5rem] text-lg">
@@ -44,6 +42,6 @@ export default function page() {
           </div>
         </div>
       </div>
-    </section>
+    </section >
   );
 }
